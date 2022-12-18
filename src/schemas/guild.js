ca41a257532fc57c552b4351604324534281e46f
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2022-12-18 23:14:19 +0100

    Create guild.js

---
+++ src/schemas/guild.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+const guildSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    guildId: String,
+    guildName: String,
+    guildIcon: { type: String, required: false },
+    serverRoleId: String,
+    supportRoleId: Array,
+    extraRoleId: Array,
+    setupDate: String,
+    welcomeChannelId: String,
+    userRoleId: String,
+}, {
+    versionKey: false
+});
+
+module.exports = model('Guild', guildSchema, 'guilds');

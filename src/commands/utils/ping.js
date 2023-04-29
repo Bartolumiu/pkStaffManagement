8c2c65b0c86f4bb2015356fa9dd789715033c7a0
Bartolumiu <mario04gb@gmail.com>
2023-04-29 15:32:50 +0000

    Multiple changes

--- src/commands/utils/ping.js
+++ src/commands/utils/ping.js
@@ -3,7 +3,18 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Comprueba la latencia del bot'),
+        .setDescription('Comprueba la latencia del bot')
+        .setDescriptionLocalizations(
+            {
+                "en-GB": "Check bot latency",
+                "en-US": "Check bot latency",
+                "it": "Controllare la latenza del bot",
+                "fr": "Vérifier la latence du bot",
+                "pt-BR": "Verificar a latência do bot",
+                "de": "Bot-Latenz prüfen",
+                "no": "Sjekk bot-forsinkelse"
+            }
+        ),
     async execute(interaction, client) {
         const message = await interaction.deferReply({ fetchReply: true });
 
